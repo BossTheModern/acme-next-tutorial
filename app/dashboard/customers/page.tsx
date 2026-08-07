e130de25617a9ee60249a573070511c3d1a33ded
BossTheModern <prhotv@stud.ntnu.no>
2026-08-07 21:22:54 +0200

    implemented customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,26 @@
 import { Metadata } from 'next';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Suspense } from 'react';
+
 
 export const metadata: Metadata = {
     title: "Customers page",
 };
 
-export default function Page() {
-    return <p>Customers page</p>;
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+    }>
+}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const customers: FormattedCustomersTable[] = await fetchFilteredCustomers(query);
+
+    return (
+        <Suspense>
+            <CustomersTable customers={customers} />
+        </Suspense>
+    );
 }
